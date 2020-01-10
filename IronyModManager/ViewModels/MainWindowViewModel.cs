30979b18d5d44ef0f72b46189d27f052ffe5a43e
bcssov <nxsovereign@gmail.com>
2020-01-10 03:32:29 +0100

    Project setup

---
+++ IronyModManager/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IronyModManager.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
