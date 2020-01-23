c1901f195db16bbf30cc241d1fb317518a171cd4
bcssov <nxsovereign@gmail.com>
2020-01-23 06:58:12 +0100

    Add refresh handlers

---
+++ IronyModManager.Localization/Attributes/Handlers/LocalizationCollectionRefreshHandler.cs
@@ -0,0 +1,71 @@
+﻿// ***********************************************************************
+// Assembly         : IronyModManager.Localization
+// Author           : Mario
+// Created          : 01-23-2020
+//
+// Last Modified By : Mario
+// Last Modified On : 01-23-2020
+// ***********************************************************************
+// <copyright file="LocalizationCollectionRefreshHandler.cs" company="Mario">
+//     Mario
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IronyModManager.Localization.Attributes.Handlers
+{
+    /// <summary>
+    /// Class LocalizationCollectionRefreshHandler.
+    /// Implements the <see cref="IronyModManager.Localization.Attributes.Handlers.ILocalizationRefreshHandler" />
+    /// </summary>
+    /// <seealso cref="IronyModManager.Localization.Attributes.Handlers.ILocalizationRefreshHandler" />
+    public class LocalizationCollectionRefreshHandler : ILocalizationRefreshHandler
+    {
+        #region Methods
+
+        /// <summary>
+        /// Determines whether this instance can refresh the specified arguments.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        /// <returns><c>true</c> if this instance can refresh the specified arguments; otherwise, <c>false</c>.</returns>
+        public bool CanRefresh(LocalizationRefreshArgs args)
+        {
+            return typeof(IEnumerable<ILocalizableModel>).IsAssignableFrom(args.Property.PropertyType);
+        }
+
+        /// <summary>
+        /// Refreshes the specified arguments.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        public void Refresh(LocalizationRefreshArgs args)
+        {
+            var val = args.Property.GetValue(args.Invocation.InvocationTarget, null) as IEnumerable<ILocalizableModel>;
+            if (val?.Count() > 0)
+            {
+                foreach (var item in val)
+                {
+                    var props = item.GetType().GetProperties().Where(p => Attribute.IsDefined(p, typeof(LocalizationAttributeBase)));
+                    if (props.Count() > 0)
+                    {
+                        foreach (var prop in props)
+                        {
+                            if (prop.CanWrite)
+                            {
+                                var oldVal = prop.GetValue(item, null);
+                                ((ILocalizableModel)args.Invocation.Proxy).OnPropertyChanging(prop.Name);
+                                prop.SetValue(item, null);
+                                ((ILocalizableModel)args.Invocation.Proxy).OnPropertyChanged(prop.Name);
+                                prop.SetValue(item, oldVal);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        #endregion Methods
+    }
+}
---
+++ IronyModManager.Localization/Attributes/Handlers/LocalizationRefreshArgs.cs
@@ -0,0 +1,57 @@
+﻿// ***********************************************************************
+// Assembly         : IronyModManager.Localization
+// Author           : Mario
+// Created          : 01-23-2020
+//
+// Last Modified By : Mario
+// Last Modified On : 01-23-2020
+// ***********************************************************************
+// <copyright file="LocalizationRefreshArgs.cs" company="Mario">
+//     Mario
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System.Collections.Generic;
+using System;
+using System.Reflection;
+using Castle.DynamicProxy;
+
+namespace IronyModManager.Localization.Attributes.Handlers
+{
+    /// <summary>
+    /// Class LocalizationRefreshArgs.
+    /// </summary>
+    public class LocalizationRefreshArgs
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LocalizationRefreshArgs" /> class.
+        /// </summary>
+        /// <param name="invocation">The invocation.</param>
+        /// <param name="prop">The property.</param>
+        public LocalizationRefreshArgs(IInvocation invocation, PropertyInfo prop)
+        {
+            Property = prop;
+            Invocation = invocation;
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the invocation.
+        /// </summary>
+        /// <value>The invocation.</value>
+        public IInvocation Invocation { get; set; }
+
+        /// <summary>
+        /// Gets or sets the property.
+        /// </summary>
+        /// <value>The property.</value>
+        public PropertyInfo Property { get; set; }
+
+        #endregion Properties
+    }
+}
