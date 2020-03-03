3f1de07c948c2a5603fc17a50dfb9710d3dcf41f
bcssov <nxsovereign@gmail.com>
2020-03-03 01:57:27 +0100

    Remember installed mods search and sort state

--- src/IronyModManager.Models.Common/IPreferences.cs
+++ src/IronyModManager.Models.Common/IPreferences.cs
@@ -4,7 +4,7 @@
 // Created          : 01-11-2020
 //
 // Last Modified By : Mario
-// Last Modified On : 02-12-2020
+// Last Modified On : 03-03-2020
 // ***********************************************************************
 // <copyright file="IPreferences.cs" company="Mario">
 //     Mario
@@ -31,6 +31,24 @@ namespace IronyModManager.Models.Common
         /// </summary>
         /// <value>The game.</value>
         string Game { get; set; }
+
+        /// <summary>
+        /// Gets or sets the installed mods search term.
+        /// </summary>
+        /// <value>The installed mods search term.</value>
+        string InstalledModsSearchTerm { get; set; }
+
+        /// <summary>
+        /// Gets or sets the installed mods sort column.
+        /// </summary>
+        /// <value>The installed mods sort column.</value>
+        string InstalledModsSortColumn { get; set; }
+
+        /// <summary>
+        /// Gets or sets the installed mods sort mode.
+        /// </summary>
+        /// <value>The installed mods sort mode.</value>
+        int InstalledModsSortMode { get; set; }
 
         /// <summary>
         /// Gets or sets the locale.
