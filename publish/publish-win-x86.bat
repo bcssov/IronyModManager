461b444b03743336a9b30c89cc5ac98c8e4c852e
bcssov <nxsovereign@gmail.com>
2020-03-18 16:40:49 +0100

    Upgrade test dependencies Tweak publish profiles

--- publish/publish-win-x86.bat
+++ publish/publish-win-x86.bat
@@ -2,5 +2,3 @@ cd ..
 dotnet build --configuration Release
 dotnet publish src\IronyModManager\IronyModManager.csproj  /p:PublishProfile=src\IronyModManager\Properties\PublishProfiles\win-x86.pubxml --configuration Release
 xcopy "src\IronyModManager\bin\Release\netcoreapp3.1\win-x86\*.dll" "src\IronyModManager\bin\Release\netcoreapp3.1\publish\win-x86\" /Y /S /D
-del "src\IronyModManager\bin\Release\netcoreapp3.1\publish\win-x86\*.pdb" /S
-del "src\IronyModManager\bin\Release\netcoreapp3.1\publish\win-x86\*.xml" /S
