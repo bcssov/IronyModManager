db721378bd1ac9cbd78e28821b17c2f10f409f35
bcssov <nxsovereign@gmail.com>
2020-08-12 07:22:40 +0200

    Update publish scripts

--- publish/publish-win-x64.bat
+++ publish/publish-win-x64.bat
@@ -2,4 +2,5 @@ cd ..
 dotnet build --configuration Release
 dotnet publish src\IronyModManager\IronyModManager.csproj  /p:PublishProfile=src\IronyModManager\Properties\PublishProfiles\win-x64.pubxml --configuration Release
 xcopy "src\IronyModManager\bin\Release\netcoreapp3.1\win-x64\*.dll" "src\IronyModManager\bin\Release\netcoreapp3.1\publish\win-x64\" /Y /S /D
+xcopy "References\*.*" "src\IronyModManager\bin\Release\netcoreapp3.1\publish\win-x64\"  /Y /S /D
 cd publish
