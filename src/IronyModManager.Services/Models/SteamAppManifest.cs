cbcac0eaefa0e74e50d7db49cb45b4f7cd1be4d2
bcssov <nxsovereign@gmail.com>
2021-06-13 05:57:12 +0200

    Switch completely to vdf parser Fix some bugs

---
+++ src/IronyModManager.Services/Models/SteamAppManifest.cs
@@ -0,0 +1,37 @@
+﻿// ***********************************************************************
+// Assembly         : IronyModManager.Services
+// Author           : Mario
+// Created          : 06-13-2021
+//
+// Last Modified By : Mario
+// Last Modified On : 06-13-2021
+// ***********************************************************************
+// <copyright file="SteamAppManifest.cs" company="Mario">
+//     Mario
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace IronyModManager.Services.Models
+{
+    /// <summary>
+    /// Class SteamAppManifest.
+    /// </summary>
+    internal class SteamAppManifest
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the install dir.
+        /// </summary>
+        /// <value>The install dir.</value>
+        [JsonProperty("installdir")]
+        public string InstallDir { get; set; }
+
+        #endregion Properties
+    }
+}
---
+++ src/IronyModManager.Shared/JsonConverters/BoolConverter.cs
@@ -0,0 +1,70 @@
+﻿// ***********************************************************************
+// Assembly         : IronyModManager.Shared
+// Author           : Mario
+// Created          : 06-13-2021
+//
+// Last Modified By : Mario
+// Last Modified On : 06-13-2021
+// ***********************************************************************
+// <copyright file="BoolConverter.cs" company="Mario">
+//     Mario
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace IronyModManager.Shared.JsonConverters
+{
+    /// <summary>
+    /// Class BoolConverter.
+    /// Implements the <see cref="Newtonsoft.Json.JsonConverter" />
+    /// </summary>
+    /// <seealso cref="Newtonsoft.Json.JsonConverter" />
+    public class BoolConverter : JsonConverter
+    {
+        #region Methods
+
+        /// <summary>
+        /// Determines whether this instance can convert the specified object type.
+        /// </summary>
+        /// <param name="objectType">Type of the object.</param>
+        /// <returns><c>true</c> if this instance can convert the specified object type; otherwise, <c>false</c>.</returns>
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(bool);
+        }
+
+        /// <summary>
+        /// Reads the JSON representation of the object.
+        /// </summary>
+        /// <param name="reader">The <see cref="T:Newtonsoft.Json.JsonReader" /> to read from.</param>
+        /// <param name="objectType">Type of the object.</param>
+        /// <param name="existingValue">The existing value of object being read.</param>
+        /// <param name="serializer">The calling serializer.</param>
+        /// <returns>The object value.</returns>
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (int.TryParse(reader.Value.ToString(), out var value))
+            {
+                return value > 0;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Writes the JSON representation of the object.
+        /// </summary>
+        /// <param name="writer">The <see cref="T:Newtonsoft.Json.JsonWriter" /> to write to.</param>
+        /// <param name="value">The value.</param>
+        /// <param name="serializer">The calling serializer.</param>
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteValue(((bool)value) ? 1 : 0);
+        }
+
+        #endregion Methods
+    }
+}
