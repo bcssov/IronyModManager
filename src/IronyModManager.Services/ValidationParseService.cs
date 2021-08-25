8c88fc45d0016be7a00ada8de79b180c7daac475
scrangos <alejandrorosa@gmail.com>
2021-08-25 01:06:15 -0400

    Injection into services

---
+++ src/IronyModManager.Services/ValidationParseService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IronyModManager.Parser.Common.Parsers;
+
+namespace IronyModManager.Services
+{
+    public class ValidateParserService
+    {
+        private readonly IValidateParser validateParser;
+
+        public ValidateParserService(IValidateParser validateParser)
+        {
+            this.validateParser = validateParser;
+        }
+    }
+}
