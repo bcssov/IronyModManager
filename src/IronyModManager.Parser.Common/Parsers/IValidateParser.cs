ad46f87c940b6bd4248da311c4f51085fb760460
scrangos <alejandrorosa@gmail.com>
2021-08-25 00:19:28 -0400

    First IValidateParser interface

---
+++ src/IronyModManager.Parser.Common/Parsers/IValidateParser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IronyModManager.Parser.Common.Args;
+using IronyModManager.Shared.Models;
+
+namespace IronyModManager.Parser.Common.Parsers
+{
+    public interface IValidateParser
+    {
+        public IEnumerable<IDefinition> Validate(ParserArgs args);
+    }
+}
