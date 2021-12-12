74855f769f02347b71d7b117d4beed64bc4ec096
Nick Butcher <corsairmarks@hotmail.com>
2021-12-12 15:17:46 -0500

    Add test case for escaped quotations

--- src/IronyModManager.Parser.Tests/ModParserTests.cs
+++ src/IronyModManager.Parser.Tests/ModParserTests.cs
@@ -99,5 +99,48 @@ namespace IronyModManager.Parser.Tests
             var result = parser.Parse(sb.ToString().Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries));            
             result.FileName.Should().Be("path");            
         }
+
+        /// <summary>
+        /// Defines the test method Should_parse_mod_file.
+        /// </summary>
+        [Fact]
+        public void Should_parse_mod_file_with_escaped_quotations()
+        {
+            DISetup.SetupContainer();
+
+            var sb = new StringBuilder();
+            sb.AppendLine(@"name=""AI Species \""Limit\""""");
+            sb.AppendLine(@"path=""path""");
+            sb.AppendLine(@"user_dir=""dir""");
+            sb.AppendLine(@"replace_path=""replace""");
+            sb.AppendLine(@"tags={");
+            sb.AppendLine(@"	""Gameplay""");
+            sb.AppendLine(@"	""Fixes""");
+            sb.AppendLine(@"}");
+            sb.AppendLine(@"picture=""thumbnail.png""");
+            sb.AppendLine(@"supported_version=""2.5.*""");
+            sb.AppendLine(@"remote_file_id=""1830063425""");
+            sb.AppendLine(@"version = ""version""");
+            sb.AppendLine(@"dependencies = {");
+            sb.AppendLine(@"	""fake""");
+            sb.AppendLine(@"}");
+
+            var parser = new ModParser(new CodeParser(new Logger()));
+            var result = parser.Parse(sb.ToString().Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries));
+            result.Dependencies.Count().Should().Be(1);
+            result.Dependencies.First().Should().Be("fake");
+            result.FileName.Should().Be("path");
+            result.Name.Should().Be("AI Species \"Limit\"");
+            result.Picture.Should().Be("thumbnail.png");
+            result.RemoteId.Should().Be(1830063425);
+            result.Tags.Count().Should().Be(2);
+            result.Tags.First().Should().Be("Gameplay");
+            result.Tags.Last().Should().Be("Fixes");
+            result.Version.Should().Be("2.5.*");
+            result.UserDir.Count().Should().Be(1);
+            result.UserDir.FirstOrDefault().Should().Be("dir");
+            result.ReplacePath.Count().Should().Be(1);
+            result.ReplacePath.FirstOrDefault().Should().Be("replace");
+        }
     }
 }
